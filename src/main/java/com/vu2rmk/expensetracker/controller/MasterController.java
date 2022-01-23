16fda0ac5d4a5a9741b2a5c68d4ba60de369208b
vu2rmk <vu2rmk@gmail.com>
2022-01-23 16:53:24 +0530

    showing basic data to jsp page

--- src/main/java/com/vu2rmk/expensetracker/controller/MasterController.java
+++ src/main/java/com/vu2rmk/expensetracker/controller/MasterController.java
@@ -1,16 +1,26 @@
 package com.vu2rmk.expensetracker.controller;
 
+import com.vu2rmk.expensetracker.model.Expense;
+import com.vu2rmk.expensetracker.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class MasterController {
+
+    @Autowired
+    ExpenseService expenseService;
 
     @RequestMapping("/")
     public ModelAndView home(){
         ModelAndView mov = new ModelAndView("home");
         mov.addObject("message","List of expenses");
+        List<Expense> expenses = expenseService.findAll();
+        mov.addObject("expenses",expenses);
         return mov;
     }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,10 +8,18 @@
 </head>
 <body>
 
-<h2 title="I'm a header">The title Attribute</h2>
+    <h2 title="I'm a header">The title Attribute</h2>
 
-<p title="I'm a tooltip">Mouse over this paragraph, to display the title attribute as a tooltip.</p>
-<p>${message}</p>
+    <p title="I'm a tooltip">Mouse over this paragraph, to display the title attribute as a tooltip.</p>
+    <p>${message}</p>
+
+    <c:forEach var="expense" items="${expenses}">
+
+    <div>
+        <h3>${expense.expensename}</h3>
+        <p>&#8377; ${expense.amount}</p>
+    </div>
+    </c:forEach>
 
 </body>
 </html>
